997abc8d40b4680c10ecc025787fc0affc774469
upretiramesh78 <50411977+upretiramesh78@users.noreply.github.com>
2020-05-14 09:29:49 +0530

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello TechPrimers  Jai Gopal@@@@@@@@";
+        return "Hello TechPrimers  Jai Gopal@@@@@@@@ Haldwani";
     }
 }
